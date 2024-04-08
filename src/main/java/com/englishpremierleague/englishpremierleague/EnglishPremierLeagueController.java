91da0a2fef581cea272a43b7c8aaa2e751b466a6
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-04-08 13:28:27 +0300

    Initial commit

---
+++ src/main/java/com/englishpremierleague/englishpremierleague/EnglishPremierLeagueController.java
@@ -0,0 +1,10 @@
+package com.englishpremierleague.englishpremierleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class EnglishPremierLeague {
+
+
+
+}
---
+++ src/test/java/com/englishpremierleague/englishpremierleague/EnglishPremierLeagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.englishpremierleague.englishpremierleague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishPremierLeagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

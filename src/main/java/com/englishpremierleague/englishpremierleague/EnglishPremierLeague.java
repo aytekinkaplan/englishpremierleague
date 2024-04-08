473fcf8b2c44bab206a16044e82c497553c8df1f
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-04-08 13:29:28 +0300

    English Premier League Standings
    This project is a web application that allows you to track the information of football teams and matches in the Premier League.
    
    Technologies:
    .Java
    .Spring Boot
    .Thymeleaf
    
    Setup:
    1.Clone the project from GitHub: git clone https://github.com/aytekinkaplan/english-premier-league.git
    2.Build the project with the mvn clean install command.
    3.Run the application with the mvn spring-boot:run command.
    
    Contributing:
    To contribute to the project, you can send pull requests.

--- src/main/java/com/englishpremierleague/englishpremierleague/EnglishPremierLeague.java
+++ src/main/java/com/englishpremierleague/englishpremierleague/EnglishPremierLeague.java
@@ -1,13 +1,109 @@
 package com.englishpremierleague.englishpremierleague;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+public class EnglishPremierLeague {
+    private int rank;
+    private String teamName;
+    private int matchesPlayed;
+    private int wins;
+    private int draws;
+    private int losses;
+    private int goalsFor;
+    private int goalsAgainst;
+    private int goalDifference;
+    private int points;
 
-@SpringBootApplication
-public class EnglishPremierLeagueApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(EnglishPremierLeagueApplication.class, args);
+    // Constructor
+    public EnglishPremierLeague(int rank, String teamName, int matchesPlayed, int wins, int draws, int losses, int goalsFor, int goalsAgainst, int goalDifference, int points) {
+        this.rank = rank;
+        this.teamName = teamName;
+        this.matchesPlayed = matchesPlayed;
+        this.wins = wins;
+        this.draws = draws;
+        this.losses = losses;
+        this.goalsFor = goalsFor;
+        this.goalsAgainst = goalsAgainst;
+        this.goalDifference = goalDifference;
+        this.points = points;
     }
 
+    // Getters and setters
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public int getMatchesPlayed() {
+        return matchesPlayed;
+    }
+
+    public void setMatchesPlayed(int matchesPlayed) {
+        this.matchesPlayed = matchesPlayed;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
+    public int getDraws() {
+        return draws;
+    }
+
+    public void setDraws(int draws) {
+        this.draws = draws;
+    }
+
+    public int getLosses() {
+        return losses;
+    }
+
+    public void setLosses(int losses) {
+        this.losses = losses;
+    }
+
+    public int getGoalsFor() {
+        return goalsFor;
+    }
+
+    public void setGoalsFor(int goalsFor) {
+        this.goalsFor = goalsFor;
+    }
+
+    public int getGoalsAgainst() {
+        return goalsAgainst;
+    }
+
+    public void setGoalsAgainst(int goalsAgainst) {
+        this.goalsAgainst = goalsAgainst;
+    }
+
+    public int getGoalDifference() {
+        return goalDifference;
+    }
+
+    public void setGoalDifference(int goalDifference) {
+        this.goalDifference = goalDifference;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
 }
--- src/main/java/com/englishpremierleague/englishpremierleague/EnglishPremierLeagueController.java
+++ src/main/java/com/englishpremierleague/englishpremierleague/EnglishPremierLeagueController.java
@@ -1,10 +1,108 @@
 package com.englishpremierleague.englishpremierleague;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
-public class EnglishPremierLeague {
+@Controller
+public class EnglishPremierLeagueController {
+@GetMapping("/league")
+    public String league(Model model) {
 
+    model.addAttribute(
+            "team1", new EnglishPremierLeague(
+                    1, "Manchester City", 30, 22, 4, 4, 65, 20, 45, 70
+            )
+    );
+    model.addAttribute(
+            "team2", new EnglishPremierLeague(
+                    2, "Liverpool", 30, 20, 6, 4, 60, 25, 35, 66
+            )
+    );
+    model.addAttribute(
+            "team3", new EnglishPremierLeague(
+                    3, "Chelsea", 30, 18, 7, 5, 55, 30, 25, 61
+            )
+    );
+    model.addAttribute(
+            "team4", new EnglishPremierLeague(
+                    4, "Manchester United", 30, 17, 8, 5, 50, 30, 20, 59
+            )
+    );
+    model.addAttribute(
+            "team5", new EnglishPremierLeague(
+                    5, "Arsenal", 30, 16, 9, 5, 45, 30, 15, 57
+            )
+    );
+    model.addAttribute(
+            "team6", new EnglishPremierLeague(
+                    6, "Tottenham Hotspur", 30, 15, 8, 7, 40, 35, 5, 53
+            )
+    );
+    model.addAttribute(
+            "team7", new EnglishPremierLeague(
+                    7, "Leicester City", 30, 14, 9, 7, 38, 35, 3, 51
+            )
+    );
+    model.addAttribute(
+            "team8", new EnglishPremierLeague(
+                    8, "West Ham United", 30, 13, 10, 7, 35, 35, 0, 49
+            )
+    );
+    model.addAttribute(
+            "team9", new EnglishPremierLeague(
+                    9, "Wolverhampton Wanderers", 30, 12, 9, 9, 30, 35, -5, 45
+            )
+    );
+    model.addAttribute(
+            "team10", new EnglishPremierLeague(
+                    10, "Everton", 30, 11, 8, 11, 28, 35, -7, 41
+            )
+    );
+    model.addAttribute(
+            "team11", new EnglishPremierLeague(
+                    11, "Aston Villa", 30, 10, 10, 10, 25, 35, -10, 40
+            )
+    );
+    model.addAttribute(
+            "team12", new EnglishPremierLeague(
+                    12, "Leeds United", 30, 9, 8, 13, 22, 40, -18, 35
+            )
+    );
+    model.addAttribute(
+            "team13", new EnglishPremierLeague(
+                    13, "Southampton", 30, 8, 6, 16, 20, 45, -25, 30
+            )
+    );
+    model.addAttribute(
+            "team14", new EnglishPremierLeague(
+                    14, "Crystal Palace", 30, 6, 7, 17, 15, 50, -35, 25
+            )
+    );
+    model.addAttribute(
+            "team15", new EnglishPremierLeague(
+                    15, "Newcastle United", 30, 5, 9, 16, 12, 55, -43, 24
+            )
+    );
+    model.addAttribute(
+            "team16", new EnglishPremierLeague(
+                    16, "Brighton & Hove Albion", 30, 4, 8, 18, 10, 60, -50, 20
+            )
+    );
+    model.addAttribute(
+            "team17", new EnglishPremierLeague(
+                    17, "Burnley", 30, 3, 10, 17, 8, 65, -57, 19
+            )
+    );
+    model.addAttribute(
+            "team18", new EnglishPremierLeague(
+                    18, "Fulham", 30, 2, 6, 22, 5, 70, -65, 12
+            )
+    );
+
+
+        return "league";
+    }
 
 
 }
